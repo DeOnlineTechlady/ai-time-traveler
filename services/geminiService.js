ae00e6cde8dd3566ef46ce54307408f7fa955d42
DeOnlineTechlady <saskia@deonlinetechlady.nl>
2025-11-21 11:42:23 -0800

    Create geminiService.js

---
+++ services/geminiService.js
@@ -0,0 +1,7 @@
+export async function generateTimeTravelImage(base64Image, decade) {
+  // Placeholder: return same image and a description.
+  return {
+    url: base64Image,
+    description: 'Placeholder time travel result for ' + decade + '.'
+  };
+}
